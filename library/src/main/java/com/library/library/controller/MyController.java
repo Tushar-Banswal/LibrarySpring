8fadd8560d2529801330e728eaf921b4b0864cfb
Tushar-Banswal <tusharbanswal2003@gmail.com>
2023-09-30 19:40:02 +0530

    added api functionality

--- library/src/main/java/com/library/library/controller/MyController.java
+++ library/src/main/java/com/library/library/controller/MyController.java
@@ -28,4 +28,20 @@ public class MyController {
 	{
 	   return this.courseService.getCourses();
 	}
+
+	//get books through api's
+	@GetMapping(value = "/books")
+	public List<Object> getBooks() {
+		
+		Map<String, String> params = new HashMap<>();
+		params.put("X-Rapidapi-Key", "14ecb7ca5amshbc6f11972fd65eap11ea23jsn7b5659b242a2");
+		params.put("X-Rapidapi-Host", "book-finder1.p.rapidapi.com");
+
+		String url = "https://book-finder1.p.rapidapi.com/api/search";
+
+		RestTemplate restTemp = new RestTemplate();
+		Object[] results = restTemp.getForObject(url, Object[].class, params);
+
+		return Array.asList(results);
+	}
 }
